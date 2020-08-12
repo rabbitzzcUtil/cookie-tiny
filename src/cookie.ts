1a18ab73b67fe93fa04f2f42265101dd479f3fe4
Rabbitzzc <zzclovelcs@gmail.com>
2020-08-12 14:49:25 +0800

    feat: 🎉 finish core

--- src/cookie.ts
+++ src/cookie.ts
@@ -1,20 +1,106 @@
 
-import { isObject } from './lib/utils';
+import { isObject, encode, decode } from './lib/utils'
 
 class Cookie {
     cookie: string
     constructor() {
         this.cookie = document.cookie
     }
-    public set(key: string | object, value: any, options?: object): void {
+    public set(key: any, value: any, options?: any): Cookie {
         if (isObject(key)) {
-            Object.keys(key).forEach(v => this.set(v, key[v], value))
+            for (const v in key) {
+                if (key.hasOwnProperty(v)) this.set(v, key[v], options)
+            }
+        } else {
+            const inOptions = options || { path: '/' }
+
+            if (typeof inOptions.expires === 'number') {
+                inOptions.expires = new Date(Date.now() + inOptions.expires * 864e5)
+            }
+
+            let expires = inOptions.expires ? inOptions.expires : ''
+            const expiresType = typeof expires
+
+            if (expiresType && expiresType === 'string') {
+                expires = new Date(expires)
+            } else if (expiresType === 'number') {
+                // ! 因为IE不支持“max-age”cookie属性
+                expires = new Date(Date.now() + (expires as unknown as number) * 864e5)
+            }
+
+            if (expires) {
+                inOptions.expires = (expires as Date).toUTCString()
+                expires = ';expires=' + (expires as Date).toUTCString()
+            }
+
+            // 对其他的参数进行匹配
+            /**
+             * path
+             * domain
+             * secure
+             * sameSite
+             */
+            let train = ''
+            for (const o in inOptions) {
+                if ((inOptions[o])) {
+                    train += '; ' + o
+                    // secure 配置
+                    if (inOptions[o] === true) continue
+                    train += '=' + inOptions[o].split(';')[0]
+                }
+            }
+            // 对 key 与 value 进行 ASCII 等装换
+            key = encode(key)
+            value = encode(value)
+
+            document.cookie = key + '=' + value + train
         }
+        return this
     }
 
-    public get() { }
+    public get(key: string | string[]) {
+        const cookies = this.all()
 
-    public remove() { }
+        if (!Array.isArray(key as string)) return cookies[key]
+
+        const result = {}
+
+        key.forEach((v: string | number) => {
+            result[v] = cookies[v]
+        })
+
+        return result
+    }
+
+
+    public all(): object {
+        if (document.cookie === '') return {}
+
+        const result: any = {}
+        const cookies = document.cookie ? document.cookie.split('; ') : []
+
+        cookies.forEach(v => {
+            // ['a', 'b=b=b=b=b=b=b']
+            const parts: any[] = v.split('=')
+            const key = decode(parts.shift())
+            const value = decode(parts.join('='))
+            result[key] = value
+        })
+
+        return result
+    }
+
+    public remove(key: any) {
+        const keys = Array.isArray(key) ? key : [key]
+        keys.forEach(v => this.set(v, '', { expires: -1 }))
+        return this
+    }
+
+    public clear() {
+        return this.remove(Object.keys(this.all()))
+    }
+
+
 }
 
 
@@ -26,6 +112,7 @@ class Cookie {
 
 let cookie = null
 if (typeof document === "undefined") {
+    // tslint:disable-next-line:no-console
     console.error('The library is not supported in the current environment')
 } else {
     cookie = new Cookie()
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,3 +1,14 @@
-export const isObject: (obj: any) => boolean = (obj:any): obj is object => {
+export const isObject: (obj: any) => boolean = (obj: any): obj is object => {
     return !!obj && Object.prototype.toString.call(obj) === '[object Object]'
+}
+
+
+export const encode = (str: string): string => {
+    return String(str).replace(/[,;"\\=\s%]/g, char => {
+        return encodeURIComponent(char)
+    })
+}
+
+export const decode = (str: string): string => {
+    return decodeURIComponent(str)
 }
